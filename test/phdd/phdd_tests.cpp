6892c64a1ddc9e234aec4bbf39e58fa86bf80198
jschramm <jschramm@uni-bremen.de>
2024-09-09 16:34:27 +0200

    minifloat test

--- test/phdd/phdd_tests.cpp
+++ test/phdd/phdd_tests.cpp
@@ -21,139 +21,24 @@ using namespace freddy;
 // Functions
 // *********************************************************************************************************************
 
+static auto int_to_bool_vec(int n, int size) -> std::vector<bool>
+{
+    std::vector<bool> result;
+    for(int bit_mask = 1; bit_mask < (1 << size); bit_mask <<= 1)
+    {
+        result.push_back((n & bit_mask) != 0);
+    }
+    return result;
+}
+
 // *********************************************************************************************************************
 // Macros
 // *********************************************************************************************************************
 
-TEST_CASE("sphdd_constants", "[phdd, constants]")
+TEST_CASE("sphdd_integer", "[phdd, integer]")
 {
-    dd::phdd_manager mgr;
+    // representation of 5bit integer including addition, subtraction, multiplication
 
-    auto x = mgr.var("x");
-    auto y = mgr.var("y");
-
-    SECTION("Constants with own member functions")
-    {
-        std::ofstream fs("../phdd_constants_1.dot");
-        mgr.print({mgr.zero(), mgr.one(), mgr.two()}, {"zero()", "one()", "two()"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Constants over mgr.constant")
-    {
-        std::ofstream fs2("../phdd_constants_2.dot");
-        mgr.print({mgr.constant(0), mgr.constant(1), mgr.constant(2), mgr.constant(3), mgr.constant(4), mgr.constant(5),
-                   mgr.constant(6), mgr.constant(7), mgr.constant(8), mgr.constant(9), mgr.constant(10), mgr.constant(11)},
-                  {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"}, fs2);
-        fs2.close();
-        REQUIRE(true);
-    }
-}
-
-TEST_CASE("sphdd_addition", "[phdd, addition]")
-{
-    dd::phdd_manager mgr;
-    auto x = mgr.var("x");
-    auto y = mgr.var("y");
-
-    SECTION("Add up constants")
-    {
-        std::ofstream fs("../phdd_add_const+consts.dot");
-        mgr.print({mgr.constant(18), mgr.constant(14),
-                   mgr.constant(18) + mgr.constant(14)},
-                  {"18", "14", "18+14"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Add up var and constants")
-    {
-        std::ofstream fs("../phdd_add_var+consts.dot");
-        mgr.print({mgr.zero() + x, mgr.one() + x, mgr.two() + x},
-                  {"0+x", "1+x", "2+x"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Add up vars")
-    {
-        std::ofstream fs("../phdd_add_var+var.dot");
-        mgr.print({x + x, y + x, y + y}, {"x+x", "y+x", "y+y"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Add linear function 3x+5y+5")
-    {
-        auto f = x + x + x + y + y + y + y + y + mgr.constant(5);
-
-        std::ofstream fs("../phdd_add_lin.dot");
-        mgr.print({f}, {"3x+5y+5"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-}
-
-TEST_CASE("sphdd_multiplication", "[phdd, multiplication]")
-{
-    dd::phdd_manager mgr;
-    auto x = mgr.var("x");
-    auto y = mgr.var("y");
-
-    SECTION("Multiply constants")
-    {
-        std::ofstream fs("../phdd_mult_const*consts.dot");
-        mgr.print({ mgr.constant(6) * mgr.constant(3),
-                    mgr.constant(8) * mgr.constant(8),
-                    mgr.constant(3) * mgr.constant(3)},
-                  {"6*3", "8*8", "3*3"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Multiply constants with vars")
-    {
-        std::ofstream fs("../phdd_mult_const*vars.dot");
-        mgr.print({mgr.constant(6) * x,
-                   mgr.constant(16) * x,
-                   mgr.constant(3) * x},
-                  {"6x", "16x", "3x"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Multiply vars with vars")
-    {
-        std::ofstream fs("../phdd_mult_vars*vars.dot");
-        mgr.print({x * x,
-                   y * x,
-                   y * y},
-                  {"xx", "xy", "yy"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-
-    SECTION("Multiply polynomials")
-    {
-        auto f1 = (mgr.two() * x) + (mgr.one() * y) + mgr.two();
-
-        auto f2 = (mgr.two() * x) + (mgr.two() * y) + mgr.two();
-
-        std::ofstream fs("../phdd_mult_poly.dot");
-        mgr.print({f1,
-                   f2,
-                   f1 * f2},
-                  {"2x+y+2", "2x+2y+2",
-                   "4xx + 6xy + 8x + 2yy + 6y + 4\n"
-                   "12x 6xy 8y 4"}, fs);
-        fs.close();
-        REQUIRE(true);
-    }
-}
-
-TEST_CASE("sphdd_integer", "[phdd, int, addition]")
-{
     dd::phdd_manager mgr;
     auto x0 = mgr.var("x0"); auto y0 = mgr.var("y0"); auto p0 = mgr.constant( 1);
     auto x1 = mgr.var("x1"); auto y1 = mgr.var("y1"); auto p1 = mgr.constant( 2);
@@ -168,22 +53,12 @@ TEST_CASE("sphdd_integer", "[phdd, int, addition]")
     auto prod = x*y;
     auto diff = x-y;
 
-    auto int_to_bool_vec5 = [](int n) -> std::vector<bool>
-    {
-        std::vector<bool> result;
-        for(int bit_mask = 1; bit_mask < 32; bit_mask <<= 1)
-        {
-            result.push_back((n & bit_mask) != 0);
-        }
-        return result;
-    };
-
     for(int i = 0; i < 32; i++)
     {
-        auto x_assignment= int_to_bool_vec5(i);
+        auto x_assignment= int_to_bool_vec(i,5);
         for(int j = 0; j < 32; j++)
         {
-            auto y_assignment = int_to_bool_vec5(j);
+            auto y_assignment = int_to_bool_vec(j,5);
             std::vector<bool> assignment;
             for(unsigned int k = 0; k < x_assignment.size(); k++)
             {
@@ -195,6 +70,59 @@ TEST_CASE("sphdd_integer", "[phdd, int, addition]")
             REQUIRE(sum.eval(assignment) == i+j);
             REQUIRE(prod.eval(assignment) == i*j);
             REQUIRE(diff.eval(assignment) == i-j);
+        }
+    }
+}
+
+
+TEST_CASE("sphdd_minifloat", "[phdd, mini-float]")
+{
+    // representing a 8bit mini float:
+    // 1 sign bit, 4 exponent bits, 3 significant bits
+    // no hidden bit, bias = 7
+
+    dd::phdd_manager mgr;
+    auto x_sg = mgr.var("x_sg"); //auto y_sg = mgr.var("y_sg");
+    auto x_e0 = mgr.var("x_e0"); //auto y_e0 = mgr.var("y_e0");
+    auto x_e1 = mgr.var("x_e1"); //auto y_e1 = mgr.var("y_e1");
+    auto x_e2 = mgr.var("x_e2"); //auto y_e2 = mgr.var("y_e2");
+    auto x_e3 = mgr.var("x_e3"); //auto y_e3 = mgr.var("y_e3");
+    auto x_m0 = mgr.var("x_m0"); //auto y_m0 = mgr.var("y_m0");
+    auto x_m1 = mgr.var("x_m1"); //auto y_m1 = mgr.var("y_m1");
+    auto x_m2 = mgr.var("x_m2"); //auto y_m2 = mgr.var("y_m2");
+
+    auto x_s = (-mgr.two() * x_sg) + mgr.one();
+    auto x_m = (mgr.constant(4) * x_m2) +
+               (mgr.constant(2) * x_m1) +
+               (mgr.constant(1) * x_m0);
+    auto x_e = (mgr.constant(255) * x_e3 + mgr.one()) *
+               (mgr.constant( 15) * x_e2 + mgr.one()) *
+               (mgr.constant(  3) * x_e1 + mgr.one()) *
+               (mgr.constant(  1) * x_e0 + mgr.one()) *
+               (mgr.constant(0.0078125)); // 2^(-7)
+    auto x = x_s * x_m * x_e;
+
+        std::ofstream fs1("../phdd_minifloat.dot");
+        mgr.print({x,x_s,x_e,x_m}, {"x","s","e","m"}, fs1);
+        fs1.close();
+
+
+    for(int const s : {0,1})
+    {
+        for(int e = 0; e < 16; e++)
+        {
+            for(int m  = 0; m < 8; m++)
+            {
+                auto assignment = std::vector<bool>{(bool)s};
+                auto e_assignment = int_to_bool_vec(e,4);
+                auto m_assignment = int_to_bool_vec(m,3);
+                for(auto b : e_assignment) assignment.push_back(b);
+                for(auto b : m_assignment) assignment.push_back(b);
+
+                auto val = x.eval(assignment);
+                auto spec = pow(-1,s) * m * pow(2, e-7);
+                assert(val == spec);
+            }
         }
     }
 }
