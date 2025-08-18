e3460d19153d7d05a65bbe488389f4858caf41b2
Rune Krauss <5829946+runekrauss@users.noreply.github.com>
2025-08-18 17:40:47 +0200

    Update variable again

--- include/freddy/detail/variable.hpp
+++ include/freddy/detail/variable.hpp
@@ -74,7 +74,7 @@ class variable final
   private:
     friend manager<E, V>;
 
-    friend auto operator<<(std::ostream&, manager<E, V> const&) -> std::ostream&;
+    friend auto operator<< <>(std::ostream&, manager<E, V> const&) -> std::ostream&;
 
     expansion t;  // decomposition type
 
