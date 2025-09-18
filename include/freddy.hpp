a2c8cdb4c8afcf327ea6bbb9ca977d06f80c0d74
Rune Krauss <5829946+runekrauss@users.noreply.github.com>
2025-09-18 21:12:10 +0200

    Add umbrella header

---
+++ include/freddy.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// *********************************************************************************************************************
+// Includes
+// *********************************************************************************************************************
+
+#include "freddy/config.hpp"     // manager configuration
+#include "freddy/dd/add.hpp"     // algebraic decision diagram
+#include "freddy/dd/bdd.hpp"     // binary decision diagram
+#include "freddy/dd/bhd.hpp"     // binary hybrid diagram
+#include "freddy/dd/bmd.hpp"     // binary moment diagram
+#include "freddy/expansion.hpp"  // expansion types
