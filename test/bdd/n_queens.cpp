e3937bae672012c5e190a9d236850efe112495ae
Rune Krauss <5829946+runekrauss@users.noreply.github.com>
2025-09-23 00:47:59 +0200

    Update n-queens

--- test/bdd/n_queens.cpp
+++ test/bdd/n_queens.cpp
@@ -4,7 +4,8 @@
 
 #include <catch2/catch_test_macros.hpp>  // TEST_CASE
 
-#include <freddy/dd/bdd.hpp>  // dd::bdd
+#include <freddy/config.hpp>  // config
+#include <freddy/dd/bdd.hpp>  // bdd_manager
 
 #include <cassert>  // assert
 #include <cstdint>  // std::int32_t
@@ -23,11 +24,12 @@ namespace
 // Functions
 // =====================================================================================================================
 
-auto enc(std::int32_t const n, dd::bdd_manager& mgr)  // NOLINT(readability-function-cognitive-complexity)
+auto queens(std::int32_t const n, bdd_manager& mgr)
 {
     assert(n > 0);
 
-    std::vector<std::vector<dd::bdd>> x(n, std::vector<dd::bdd>(n));
+    // initialization
+    std::vector<std::vector<bdd>> x(n, std::vector<bdd>(n));
     for (auto i = 0; i < n; ++i)
     {
         for (auto j = 0; j < n; ++j)
@@ -36,54 +38,75 @@ auto enc(std::int32_t const n, dd::bdd_manager& mgr)  // NOLINT(readability-func
         }
     }
 
-    auto pred = mgr.one();
+    // two queens must not be in the same row
+    auto horiz_constr = [n, &mgr, &x](std::int32_t const i, std::int32_t const j) {
+        auto res = mgr.one();
+        for (auto k = 0; k < n; ++k)
+        {
+            if (k != j)
+            {
+                res &= ~(x[i][j] & x[i][k]);
+            }
+        }
+        return res;
+    };
+
+    // two queens must not be in the same column
+    auto vert_constr = [n, &mgr, &x](std::int32_t const i, std::int32_t const j) {
+        auto res = mgr.one();
+        for (auto k = 0; k < n; ++k)
+        {
+            if (k != i)
+            {
+                res &= ~(x[i][j] & x[k][j]);
+            }
+        }
+        return res;
+    };
+
+    // two queens must not be on the same up-right diagonal
+    auto up_diag_constr = [n, &mgr, &x](std::int32_t const i, std::int32_t const j) {
+        auto res = mgr.one();
+        for (auto k = 0; k < n; ++k)
+        {
+            auto const l = j + k - i;
+            if (l >= 0 && l < n && k != i)
+            {
+                res &= ~(x[i][j] & x[k][l]);
+            }
+        }
+        return res;
+    };
+
+    // two queens must not be on the same down-right diagonal
+    auto down_diag_constr = [n, &mgr, &x](std::int32_t const i, std::int32_t const j) {
+        auto res = mgr.one();
+        for (auto k = 0; k < n; ++k)
+        {
+            auto const l = j + i - k;
+            if (l >= 0 && l < n && k != i)
+            {
+                res &= ~(x[i][j] & x[k][l]);
+            }
+        }
+        return res;
+    };
+
+    auto pred = mgr.one();  // predicate
     for (auto i = 0; i < n; ++i)
     {
-        auto tmp = mgr.zero();
+        auto row_existence = mgr.zero();
         for (auto j = 0; j < n; ++j)
         {
-            // two queens must not be in the same row
-            for (auto k = 0; k < n; ++k)
-            {
-                if (k != j)
-                {
-                    pred &= ~(x[i][j] & x[i][k]);
-                }
-            }
+            pred &= horiz_constr(i, j);
+            pred &= vert_constr(i, j);
+            pred &= up_diag_constr(i, j);
+            pred &= down_diag_constr(i, j);
 
-            // two queens must not be in the same column
-            for (auto k = 0; k < n; ++k)
-            {
-                if (k != i)
-                {
-                    pred &= ~(x[i][j] & x[k][j]);
-                }
-            }
-
-            // two queens must not be along an up right diagonal
-            for (auto k = 0; k < n; ++k)
-            {
-                auto const l = j + k - i;
-                if (l >= 0 && l < n && k != i)
-                {
-                    pred &= ~(x[i][j] & x[k][l]);
-                }
-            }
-
-            // two queens must not be along a down right diagonal
-            for (auto k = 0; k < n; ++k)
-            {
-                auto const l = j + i - k;
-                if (l >= 0 && l < n && k != i)
-                {
-                    pred &= ~(x[i][j] & x[k][l]);
-                }
-            }
-
-            // there must be a queen in each row globally
-            tmp |= x[i][j];
+            // there must be a queen in each row
+            row_existence |= x[i][j];
         }
-        pred &= tmp;
+        pred &= row_existence;
     }
     return pred;
 }
@@ -96,35 +119,35 @@ auto enc(std::int32_t const n, dd::bdd_manager& mgr)  // NOLINT(readability-func
 
 TEST_CASE("1-Queens is solvable", "[queen]")
 {
-    dd::bdd_manager mgr;
+    bdd_manager mgr{config{.init_var_cap = 1}};
 
-    CHECK(enc(1, mgr).sharpsat() == 1);
+    CHECK(queens(1, mgr).sharpsat() == 1);
 }
 
 TEST_CASE("2-Queens is unsolvable", "[queen]")
 {
-    dd::bdd_manager mgr;
+    bdd_manager mgr{{.init_var_cap = 4}};
 
-    CHECK(enc(2, mgr).sharpsat() == 0);
+    CHECK(queens(2, mgr).sharpsat() == 0);
 }
 
 TEST_CASE("3-Queens is unsolvable", "[queen]")
 {
-    dd::bdd_manager mgr;
+    bdd_manager mgr{{.init_var_cap = 9}};
 
-    CHECK(enc(3, mgr).sharpsat() == 0);
+    CHECK(queens(3, mgr).sharpsat() == 0);
 }
 
 TEST_CASE("4-Queens is solvable", "[queen]")
 {
-    dd::bdd_manager mgr;
+    bdd_manager mgr{{.init_var_cap = 16}};
 
-    CHECK(enc(4, mgr).sharpsat() == 2);
+    CHECK(queens(4, mgr).sharpsat() == 2);
 }
 
 TEST_CASE("5-Queens is solvable", "[queen]")
 {
-    dd::bdd_manager mgr;
+    bdd_manager mgr{{.init_var_cap = 25}};
 
-    CHECK(enc(5, mgr).sharpsat() == 10);
+    CHECK(queens(5, mgr).sharpsat() == 10);
 }
