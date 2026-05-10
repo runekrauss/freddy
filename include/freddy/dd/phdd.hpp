3f4f6f3b87767dab02ed6d17a97eec82a20a29d6
Marius <48012819+MariusMarMarius@users.noreply.github.com>
2026-05-11 01:31:07 +0200

    clang-format error fix

--- include/freddy/dd/phdd.hpp
+++ include/freddy/dd/phdd.hpp
@@ -30,9 +30,11 @@
 // Namespaces
 // *********************************************************************************************************************
 
-struct phdd_weight  // NOLINT(misc-non-private-member-variables-in-classes)
+struct phdd_weight
 {
+    // NOLINT(misc-non-private-member-variables-in-classes)
     bool x;
+    // NOLINT(misc-non-private-member-variables-in-classes)
     std::int32_t y;
 
     auto operator<=>(const phdd_weight&) const = default;
