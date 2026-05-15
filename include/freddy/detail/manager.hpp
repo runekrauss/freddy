bc676370324fc111af830f9db78d2ff9effd8760
Marvin Hindmarsh <163017290+mhindmarsh@users.noreply.github.com>
2026-05-15 17:44:28 +0200

    refactor: add templated dtl_sift chain on detail::manager

--- include/freddy/detail/manager.hpp
+++ include/freddy/detail/manager.hpp
@@ -821,6 +821,29 @@ class manager
         gc();
     }
 
+    template <typename DD>
+    void dtl_sift(std::vector<DD> const& fs)
+    {
+        auto comp_largest_layer = [this](var_index const x, var_index const y) {
+            return vlist[x].ntable.size() > vlist[y].ntable.size();
+        };
+
+        gc();
+
+        std::vector<var_index> tmp_vars(var_count());
+        for (var_index x = 0; x < var_count(); ++x)
+        {
+            tmp_vars[x] = x;
+        }
+        gc();
+        std::ranges::sort(tmp_vars, comp_largest_layer);
+        for (var_index i = 0; i < var_count(); ++i)
+        {
+            dtl_sift_single_var(tmp_vars[i], fs);
+        }
+        gc();
+    }
+
     auto dump_dot(std::vector<edge_ptr> const& fs, std::vector<std::string> const& outputs, std::ostream& os) const
     {
         assert(outputs.empty() ? true : outputs.size() == fs.size());
@@ -918,6 +941,33 @@ class manager
         return res;
     }
 
+    template <typename DD>
+    auto dtl_find_smallest_level(dtl_sift_result const& curr_best, expansion const exp, std::vector<DD> const& fs)
+    {
+        auto res = curr_best;
+        auto const x = curr_best.x;
+        sift(var2lvl[x], static_cast<var_index>(var_count() - 1));  // move to the bottom
+        change_decomposition(x, exp);
+        auto const exceeding_size = static_cast<double>(dtl_get_size(fs)) * cfg.max_node_growth;
+        for (auto i = var2lvl[x]; i > 0; --i)
+        {
+            sift(i, i - 1);
+            gc();
+            auto const current_size = dtl_get_size(fs);
+            if (static_cast<double>(current_size) > exceeding_size)
+            {
+                break;
+            }
+            if (current_size < res.size)
+            {
+                res.size = current_size;
+                res.pos = var2lvl[x];
+                res.exp = vlist[x].t;
+            }
+        }
+        return res;
+    }
+
     [[nodiscard]] auto dtl_get_size(std::vector<edge_ptr> const& fs) const
     {
         if (fs.empty())
@@ -927,7 +977,39 @@ class manager
         return size(fs);
     }
 
+    template <typename DD>
+    [[nodiscard]] auto dtl_get_size(std::vector<DD> const& fs) const
+    {
+        if (fs.empty())
+        {
+            return node_count();
+        }
+        return size(fs);  // calls templated size<DD>
+    }
+
     auto dtl_sift_single_var(var_index const x, std::vector<edge_ptr> const& fs)
+    {
+        dtl_sift_result res{.x = x, .pos = var2lvl[x], .size = dtl_get_size(fs), .exp = vlist[x].t};
+
+        // find smallest level for Shannon
+        res = dtl_find_smallest_level(res, expansion::S, fs);
+
+        // find smallest level for positive Davio
+        res = dtl_find_smallest_level(res, expansion::pD, fs);
+
+        // find smallest level for negative Davio
+        res = dtl_find_smallest_level(res, expansion::nD, fs);
+
+        // move variable to smallest level with smallest expansion type
+        sift(var2lvl[x], static_cast<var_index>(var_count() - 1));
+        change_decomposition(x, res.exp);
+        sift(var2lvl[x], res.pos);
+
+        return res;
+    }
+
+    template <typename DD>
+    auto dtl_sift_single_var(var_index const x, std::vector<DD> const& fs)
     {
         dtl_sift_result res{.x = x, .pos = var2lvl[x], .size = dtl_get_size(fs), .exp = vlist[x].t};
 
